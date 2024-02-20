edbcf3699436a0f1d26ab2b9b99bd83faa75abc2
Shoirhi <dibella.flqxl@simplelogin.com>
2024-02-20 16:42:45 +0900

    robots.txtとsitemapを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bolstatech.com',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
-    icon()
+    icon(),
+    sitemap(),
   ],
 });
